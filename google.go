06063a6d518af153e65b910e7fe7a64880bde56a
JP Robinson <john.paul.robinson@gmail.com>
2018-01-11 00:20:03 -0500

    wip

--- google.go
+++ google.go
@@ -3,13 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 
+	"github.com/NYTimes/marvin"
 	"github.com/jprobinson/dialogflow"
 	"github.com/jprobinson/gosubway"
 )
@@ -26,7 +29,7 @@ func (g *google) Actions() map[string]dialogflow.GoogleActionHandler {
 	return map[string]dialogflow.GoogleActionHandler{
 		"my_next_train_request":      g.myTrain,
 		"my_following_train_request": g.myFollowingTrain,
-		"save_my_stop_request":       g.saveMyStop,
+		"save_my_stop_request":       g.saveMyStopAction,
 		"next_train_request":         g.nextTrain,
 		"following_train_request":    g.followingTrain,
 	}
@@ -34,45 +37,106 @@ func (g *google) Actions() map[string]dialogflow.GoogleActionHandler {
 
 func (g *google) myTrain(ctx context.Context, r *dailogflow.GoogleRequest) (*dialogflow.GoogleFulfillmentResponse, error) {
 	uid := r.OriginalRequest.Data.User.UserID
-
-	var res string
-	switch { 
-	case :
 	if uid == "" {
-		res = "sorry, you need to be logged in for that to work"
+		return simpleGoogleResponse("Sorry, you need to be logged in for that to work")
 	}
-	mys, serr := getMyStop(ctx, uid)
-	if serr == datastore.ErrNoSuchEntity {
-		res = "It looks like you haven't saved your personalized subway stop yet! Ask NYC Train Time to \"save my stop\" to create or update your stop."
+	mys, err := getMyStop(ctx, uid)
+	if err == datastore.ErrNoSuchEntity {
+		return simpleGoogleResponse(
+			"It looks like you haven't saved your personalized subway stop yet! Ask NYC Train Time to \"save my stop\" to create or update your stop.")
 	}
-	if serr != nil {
-		err = serr
-		res = "sorry, we were unable to look up your stop."
-		break
+	if err != nil {
+		log.Debugf(ctx, "unable to get my stop: %s", err)
+		return simpleGoogleResponse(
+			"Sorry, we were unable to access the train feed. Please try again")
 	}
+
 	ft, err := parseFeed(mys.Line)
 	if err != nil {
 		log.Debugf(ctx, "unable to parse line: %s", mys.Line)
-		res = fmt.Sprintf("sorry, the %s line is not available yet", mys.Line)
-		break
+		return simpleGoogleResponse(
+			fmt.Sprintf("sorry, the %s line is not available yet", mys.Line))
 	}
-	res = s.getNextTrainDialog(ctx, ft, mys.Line, mys.Stop, mys.Dir)
+
+	return simpleGoogleResponse(
+		s.getNextTrainDialog(ctx, ft, mys.Line, mys.Stop, mys.Dir))
 }
 
 func (g *google) myFollowingTrain(ctx context.Context, r *dailogflow.GoogleRequest) (*dialogflow.GoogleFulfillmentResponse, error) {
+	uid := r.OriginalRequest.Data.User.UserID
+	if uid == "" {
+		return simpleGoogleResponse("Sorry, you need to be logged in for that to work")
+	}
+	mys, err := getMyStop(ctx, uid)
+	if err == datastore.ErrNoSuchEntity {
+		return simpleGoogleResponse(
+			"It looks like you haven't saved your personalized subway stop yet! Ask NYC Train Time to \"save my stop\" to create or update your stop.")
+	}
+	if err != nil {
+		log.Debugf(ctx, "unable to get my stop: %s", err)
+		return simpleGoogleResponse(
+			"Sorry, we were unable to access the train feed. Please try again")
+	}
 
+	ft, err := parseFeed(mys.Line)
+	if err != nil {
+		log.Debugf(ctx, "unable to parse line: %s", mys.Line)
+		return simpleGoogleResponse(
+			fmt.Sprintf("sorry, the %s line is not available yet", mys.Line))
+	}
+
+	return simpleGoogleResponse(
+		s.getFollowingTrainDialog(ctx, ft, mys.Line, mys.Stop, mys.Dir))
 }
 
-func (g *google) saveMyStop(ctx context.Context, r *dailogflow.GoogleRequest) (*dialogflow.GoogleFulfillmentResponse, error) {
+func (g *google) saveMyStopAction(ctx context.Context, r *dailogflow.GoogleRequest) (*dialogflow.GoogleFulfillmentResponse, error) {
+	uid := r.OriginalRequest.Data.User.UserID
+	if uid == "" {
+		return simpleGoogleResponse("Sorry, you need to be logged in for that to work")
+	}
 
+	line := strings.ToUpper(r.Result.Parameters["subway-line"].(string))
+	stop := r.Result.Parameters["subway-stop"].(string)
+	dir := r.Result.Parameters["subway-direction"].(string)
+
+	err = saveMyStop(ctx, uid, line, stop, dir)
+	if err != nil {
+		return nil, marvin.NewJSONStatusResponse(map[string]string{
+			"error": "unable to complete request: " + err.Error(),
+		}, http.StatusInternalServerError)
+	}
+	return simpleGoogleResponse(fmt.Sprintf(
+		"Successfully saved your stop, %s bound %s trains at %s. To update your stop again, ask NYC Train Time to \"save my stop\". ",
+		dir, line, stop))
 }
 
 func (g *google) nextTrain(ctx context.Context, r *dailogflow.GoogleRequest) (*dialogflow.GoogleFulfillmentResponse, error) {
+	line := strings.ToUpper(r.Result.Parameters["subway-line"].(string))
+	stop := r.Result.Parameters["subway-stop"].(string)
+	dir := r.Result.Parameters["subway-direction"].(string)
 
+	ft, err := parseFeed(line)
+	if err != nil {
+		log.Debugf(ctx, "unable to parse line: %s", line)
+		res = fmt.Sprintf("sorry, the %s line is not available yet", line)
+		break
+	}
+
+	res = s.getNextTrainDialog(ctx, ft, line, stop, dir) +
+		" If you would like me to remember your stop, ask NYC Train Time to \"save my stop\" and then ask for MY stop next time. "
 }
 
 func (g *google) followingTrain(ctx context.Context, r *dailogflow.GoogleRequest) (*dialogflow.GoogleFulfillmentResponse, error) {
-
+	line := strings.ToUpper(r.Result.Parameters["subway-line"].(string))
+	stop := r.Result.Parameters["subway-stop"].(string)
+	dir := r.Result.Parameters["subway-direction"].(string)
+	ft, err := parseFeed(line)
+	if err != nil {
+		log.Debugf(ctx, "unable to parse line: %s", line)
+		res = fmt.Sprintf("Sorry, the %s line is not available yet.", line)
+		break
+	}
+	res = s.getFollowingTrainDialog(ctx, ft, line, stop, dir)
 }
 
 func (s *google) getNextTrainDialog(ctx context.Context, ft gosubway.FeedType, line, stop, dir string) string {
@@ -134,6 +198,16 @@ func timeSpeak(t time.Time, name, line, stop, dir string) string {
 	return out
 }
 
+const source = "Where's The Train (NYC)"
+
+func simpleGoogleResponse(res string) (*dialogflow.GoogleFulfillmentResponse, error) {
+	return &dialogflow.GoogleFulfillmentResponse{
+		Speech:      res,
+		DisplayText: res,
+		Source:      source,
+	}, nil
+}
+
 type myStop struct {
 	Line string
 	Stop string
@@ -153,19 +227,4 @@ func saveMyStop(ctx context.Context, userID, line, stop, dir string) error {
 		Dir:  dir,
 	})
 	return err
-}
-
-var goodbyes = []string{
-	"Ok, bye!",
-	"Bye bye now",
-	"Peace out!",
-	"Goodbye",
-	"Hope you can catch the train!",
-	"Hope you can make it!",
-	"Adios!",
-	"Au revoir",
-	"Have a good trip!",
-	"Have a good ride!",
-	"Have a save trip!",
-	"Save travels!",
 }
